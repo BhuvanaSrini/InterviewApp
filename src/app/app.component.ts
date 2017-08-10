3686d7541c9b78377c5e88b3c054cef8e620df57
bhuvanasrini <bhuvanaaditi@gmail.com>
2017-08-10 16:11:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,Input,Output ,EventEmitter } from '@angular/core';
+import { MyNewServiceService } from './my-new-service.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(private _myService: MyNewServiceService) { }
+//test
+  public TotalExperience:any[];
+  public RelevantExperience:any[];
+  public selectedTotalExp;
+  public selectedRelevantExp;
+  public Roles:any[];
+  public selectedRoles;
+  public profile:any;
+  public btnName;
+  public TodayDate:Date;
+ onChange(newValue) {
+  debugger
+  this.profile=this._myService.getProfile();
+  this.profile.name='ll';
+   if(this.selectedRoles!=undefined)
+  this.profile.Role=this.selectedRoles.id;
+  if(this.selectedTotalExp!=undefined)
+  this.profile.TotalExperience=this.selectedTotalExp.id;
+  if(this.selectedRelevantExp!=undefined)
+  this.profile.RelevantExp=this.selectedRelevantExp.id;
+  this._myService.setProfile(this.profile);
+   }
+
+  ngOnInit() {
+    debugger
+      this.TodayDate= new Date();
+     this.TotalExperience=this._myService.getYears();
+     this.RelevantExperience=this._myService.getYears();
+     this.Roles=this._myService.getRoles();
+     this.btnName="Submit";
+     this.profile=this._myService.getProfile();
+     
+     
+
+     
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
+import { MyNewServiceService } from './my-new-service.service';
 import { AppComponent } from './app.component';
+import { SkillCompComponent } from './skill-comp/skill-comp.component';
+import { QuestionCompComponent } from './question-comp/question-comp.component';
+import { ResultCompComponent } from './result-comp/result-comp.component';
 
+const routes: Routes = [{
+  path: 'skill',
+  component: SkillCompComponent
+},
+{
+  path: 'question',
+  component: QuestionCompComponent
+},
+{
+  path: 'result',
+  component: ResultCompComponent
+}];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillCompComponent,
+    QuestionCompComponent,
+    ResultCompComponent
+ 
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
+   
   ],
-  providers: [],
+   providers: [MyNewServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
